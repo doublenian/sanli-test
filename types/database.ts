4dd093867a81d2a076a7e0c8ea2dcf0775670bb6
解念念 <doublenian.xie@gmail.com>
2025-08-05 14:42:43 +0800

    修复total_study_time类型不匹配

--- types/database.ts
+++ types/database.ts
@@ -473,7 +473,7 @@ export interface Database {
           total_passed: number;
           highest_score: number;
           average_score: number;
-          total_study_time: string;
+          total_study_time: number;
           pass_rate: number;
         };
       };
