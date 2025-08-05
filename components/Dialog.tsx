d24d8b6e9734beefa437dfb3ada40b676de40eaf
解念念 <doublenian.xie@gmail.com>
2025-08-05 15:10:49 +0800

    Updated Dialog.tsx

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native';
-import { AlertTriangle } from 'lucide-react-native';
+import { TriangleAlert as AlertTriangle } from 'lucide-react-native';
 
 interface DialogProps {
   visible: boolean;
